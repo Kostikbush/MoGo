91b502a2d4e3a2c71094b173d556eccb28689625
Kostikbush <qwerty58649@yandex.ru>
2022-04-04 10:39:27 +0300

    izi

--- js/main.js
+++ js/main.js
@@ -9,23 +9,42 @@
     };
 }());
 
+const items = document.querySelectorAll('.item__accordion');
 const mobileBtn = document.querySelector('.mob__nav-btn');
-const mobNav = document.querySelector('.mobile__nav')
+const mobNav = document.querySelector('.mobile__nav');
+
+
 mobileBtn.addEventListener('click', function () {
     mobNav.classList.toggle('mobile__nav-active');
     mobileBtn.classList.toggle('mob__nav-btn-close');
 })
+// let btn = document.querySelector('.accordion__btn');
+// let text = document.querySelector('.accordion__text-wraper');
+// let arrow = document.querySelector('.arrow__down');
+
+// btn.onclick = function () {
+//     console.log('Кнопка нажата!');
+//     text.classList.toggle('opened');
+//     arrow.classList.toggle('arrow__down-active');
+// };
 
 
+// const texts = document.querySelectorAll('.accordion__text-wraper');
+// const arrows = document.querySelectorAll('.arrow__down');
+Object.values(items).map(item => {
+    item.onclick = function (event) {
+        console.log(event.currentTarget);
 
+        const textWrapper = event.currentTarget.querySelector('.accordion__text-wraper');
 
-let btn = document.querySelector('.accordion__btn');
-let text = document.querySelector('.accordion__text-wraper');
-let arrow = document.querySelector('.arrow__down');
+        textWrapper.classList.toggle('opened');
+        
+        const arrowUp = event.currentTarget.querySelector('.arrow__down');
+        arrowUp.classList.toggle('arrow__down-active');
 
-btn.onclick = function () {
-    console.log('Кнопка нажата!');
-    text.classList.toggle('opened');
-    arrow.classList.toggle('arrow__down-active');
-};
+        // texts.classList.toggle('opened');
+        // arrows.classList.toggle('arrow__down-active');
+    };
+});
+
 
